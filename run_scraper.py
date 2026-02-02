f62af96eeafd2b360b403139709d89c72a35e3bb
LAVIT SHARMA <159767347+Lavit-sharma@users.noreply.github.com>
2026-02-02 08:54:17 +0530

    Update run_scraper.py

--- run_scraper.py
+++ run_scraper.py
@@ -101,7 +101,7 @@ log("📊 Connecting to Google Sheets...")
 try:
     gc = gspread.service_account("credentials.json")
     sheet_main = gc.open("Stock List").worksheet("Sheet1")
-    sheet_data = gc.open("Tradingview Data Reel Experimental May").worksheet("Sheet5")
+    sheet_data = gc.open("MV2 for SQL").worksheet("Sheet2")
 
     company_list = sheet_main.col_values(5)
     name_list = sheet_main.col_values(1)
